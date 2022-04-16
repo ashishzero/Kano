684af30d5d5c95fb214f6ab5c295773c85877ab1
Ashish Bhattarai <zeroa5620@gmail.com>
2022-04-16 14:19:39 +0545

    changed reverted

--- Main.cpp
+++ Main.cpp
@@ -283,18 +283,12 @@ static void json_write_value(Json_Writer *json, Interpreter *interp, Code_Type *
 			auto arr_count = ptr[0];
 			auto arr_data  = reinterpret_cast<uint8_t *>(*(size_t *)(ptr + 1));
 
-			json->begin_object();
-			json->write_key_value("count", arr_count);
-
-			json->write_key("data");
 			json->begin_array();
 			for (int64_t index = 0; index < arr_count; ++index)
 			{
 				json_write_value(json, interp, arr_type->element_type, arr_data + index * arr_type->element_type->runtime_size);
 			}
 			json->end_array();
-
-			json->end_object();
 
 			return;
 		}
@@ -304,18 +298,12 @@ static void json_write_value(Json_Writer *json, Interpreter *interp, Code_Type *
 
 			auto arr_data = (uint8_t *)data;
 
-			json->begin_object();
-			json->write_key_value("count", arr_type->element_count);
-
-			json->write_key("data");
 			json->begin_array();
 			for (int64_t index = 0; index < arr_type->element_count; ++index)
 			{
 				json_write_value(json, interp, arr_type->element_type, arr_data + index * arr_type->element_type->runtime_size);
 			}
 			json->end_array();
-
-			json->end_object();
 
 			return;
 		}
